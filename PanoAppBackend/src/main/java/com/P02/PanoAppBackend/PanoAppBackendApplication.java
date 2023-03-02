8afc441bbd46e005e709c549966edfcfb61f3f6e
ZachM <122565368+OkayHmm@users.noreply.github.com>
2023-03-02 16:50:25 -0500

    create sql connection and perform a test insertion

--- PanoAppBackend/src/main/java/com/P02/PanoAppBackend/PanoAppBackendApplication.java
+++ PanoAppBackend/src/main/java/com/P02/PanoAppBackend/PanoAppBackendApplication.java
@@ -2,12 +2,29 @@ package com.P02.PanoAppBackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
-public class PanoAppBackendApplication {
+public class PanoAppBackendApplication implements CommandLineRunner{
+
+	@Autowired
+	JdbcTemplate jdbcTemplate;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PanoAppBackendApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception
+	{
+		String sql = "INSERT INTO user (username, password, email) VALUES (?, ?, ?)";
+		int result = jdbcTemplate.update(sql, "test name", "test password", "testemail@email.com");
+
+		if (result > 0)
+			System.out.println("Insert successful.");
+		else
+			System.out.println("Insert failed.");
+	}
 }
