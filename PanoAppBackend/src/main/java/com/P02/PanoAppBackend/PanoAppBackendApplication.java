ad8a57e0b6fb06d25e837682833d7822bebc5b34
D <74151412+dv18uj@users.noreply.github.com>
2023-03-02 15:40:55 -0500

    Spring boot

---
+++ PanoAppBackend/src/main/java/com/P02/PanoAppBackend/PanoAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.P02.PanoAppBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PanoAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PanoAppBackendApplication.class, args);
+	}
+
+}
---
+++ PanoAppBackend/src/test/java/com/P02/PanoAppBackend/PanoAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.P02.PanoAppBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PanoAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
