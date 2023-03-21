6d2a14b9b0812ad969cb92d043196cd4dfbe9c03
Alexander Gibson <42209825+AlexGibby2000@users.noreply.github.com>
2023-03-21 15:34:30 -0400

    moved to PanoTour

---
+++ PanoTour/src/main/java/com/COSC4P02/PanoTour/entities/HotspotDAO.java
@@ -0,0 +1,55 @@
+package com.P02.PanoTour.entities;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.*;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+@Repository("Hotspots")
+public class HotspotDAO
+{
+    private static final EntityManagerFactory ENTITY_MANAGER_FACTORY = Persistence
+            .createEntityManagerFactory("PanoApp");
+
+    public boolean addHotspot(Hotspot hotspot) {
+        EntityManager em = ENTITY_MANAGER_FACTORY.createEntityManager();
+        EntityTransaction entityTransaction = null;
+        boolean persisted = true;
+        try {
+            entityTransaction = em.getTransaction();
+            entityTransaction.begin();
+            em.persist(hotspot);
+            entityTransaction.commit();
+        } catch (Exception exception) {
+            if (entityTransaction != null) {
+                entityTransaction.rollback();
+                persisted = false;
+            }
+        } finally {
+            em.close();
+        }
+        return persisted;
+    }
+    public boolean deleteHotspot(Hotspot hotspot) {
+        EntityManager em = ENTITY_MANAGER_FACTORY.createEntityManager();
+        EntityTransaction entityTransaction = null;
+        boolean deleted = true;
+
+        try {
+            entityTransaction = em.getTransaction();
+            entityTransaction.begin();
+            em.remove(em.contains(hotspot) ? hotspot : em.merge(hotspot));
+            entityTransaction.commit();
+        } catch (Exception e) {
+            if (entityTransaction != null) {
+                entityTransaction.rollback();
+                deleted = false;
+            }
+        } finally {
+            em.close();
+        }
+        return deleted;
+    }
+}
