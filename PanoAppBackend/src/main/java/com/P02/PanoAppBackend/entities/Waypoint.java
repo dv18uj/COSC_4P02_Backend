0b28c55afd7fe3c55f6c4cbb7e4d37a99fffc0f2
Alexander Gibson <42209825+AlexGibby2000@users.noreply.github.com>
2023-03-21 14:20:45 -0400

    Create Hotspot / HotspotDAO / Waypoint / WaypointDAO

---
+++ PanoAppBackend/src/main/java/com/P02/PanoAppBackend/entities/Waypoint.java
@@ -0,0 +1,35 @@
+package com.P02.PanoAppBackend.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "waypoint")
+public class Waypoint implements Serializable
+{
+    private static final long serialVersionUID = 338L;
+
+
+    //The foreign key of the project
+    @Column(name = "sid", nullable = false)
+    private int sid;
+
+    //The foreign key of the project
+    @Column(name = "pid", nullable = false)
+    private int pid;
+
+    @Column(name = "x", nullable = false)
+    private int x;
+
+    @Column(name = "y", nullable = false)
+    private int y;
+
+    @Column(name= "z", nullable = false)
+    private int z;
+
+    public int getPid{return pid;}
+    public int getSid() {return sid;}
+    public int getX() { return x; }
+    public int getY() { return y; }
+    public int getZ(){return z;}
+}
---
+++ PanoAppBackend/src/main/java/com/P02/PanoAppBackend/entities/WaypointDAO.java
@@ -0,0 +1,55 @@
+package com.P02.PanoAppBackend.entities;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.*;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+@Repository("Waypoints")
+public class WaypointDAO
+{
+    private static final EntityManagerFactory ENTITY_MANAGER_FACTORY = Persistence
+            .createEntityManagerFactory("PanoApp");
+
+    public boolean addWaypoint(Waypoint waypoint) {
+        EntityManager em = ENTITY_MANAGER_FACTORY.createEntityManager();
+        EntityTransaction entityTransaction = null;
+        boolean persisted = true;
+        try {
+            entityTransaction = em.getTransaction();
+            entityTransaction.begin();
+            em.persist(waypoint);
+            entityTransaction.commit();
+        } catch (Exception exception) {
+            if (entityTransaction != null) {
+                entityTransaction.rollback();
+                persisted = false;
+            }
+        } finally {
+            em.close();
+        }
+        return persisted;
+    }
+    public boolean deleteWaypoint(Waypoint waypoint) {
+        EntityManager em = ENTITY_MANAGER_FACTORY.createEntityManager();
+        EntityTransaction entityTransaction = null;
+        boolean deleted = true;
+
+        try {
+            entityTransaction = em.getTransaction();
+            entityTransaction.begin();
+            em.remove(em.contains(waypoint) ? waypoint : em.merge(waypoint));
+            entityTransaction.commit();
+        } catch (Exception e) {
+            if (entityTransaction != null) {
+                entityTransaction.rollback();
+                deleted = false;
+            }
+        } finally {
+            em.close();
+        }
+        return deleted;
+    }
+}
