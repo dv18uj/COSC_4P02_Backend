d4cc790ccb13a7ce5607524f1f32d8f47c9d7d32
samuel <samuelonabolu@gmail.com>
2023-03-30 16:04:12 -0400

    sections created

--- PanoTour/src/main/java/com/COSC4P02/PanoTour/entities/Section.java
+++ PanoTour/src/main/java/com/COSC4P02/PanoTour/entities/Section.java
@@ -1,4 +1,30 @@
 package com.COSC4P02.PanoTour.entities;
 
-public class Section {
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "section")
+public class Section implements Serializable
+{
+    private static final long serialVersionUID = 338L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int sid;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "lid")
+    private int lid;
+
+    public int getUid() { return sid; }
+    public int getLid() { return lid; }
+    public String getName() { return name; }
+    public String getDescription() { return description; }
 }
