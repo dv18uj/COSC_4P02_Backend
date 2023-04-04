9571359ea2e89a0a045c493288a4eb19002afea6
Alexander Gibson <42209825+AlexGibby2000@users.noreply.github.com>
2023-04-04 16:07:34 -0400

    updated Hotspot, HotspotController, and HotspotDAO

---
+++ PanoTour/src/main/java/com/COSC4P02/PanoTour/controllers/HotspotController.java
@@ -0,0 +1,32 @@
+package com.COSC4P02.PanoTour.controllers;
+
+
+import com.COSC4P02.PanoTour.entities.Hotspot;
+import com.COSC4P02.PanoTour.entities.HotspotDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+@RequestMapping("/api/hotspot")
+public class HotspotController {
+    private HotspotDAO hotspotDAO;
+
+    @Autowired
+    public void setHotspot(@Qualifier("Hotspots") HotspotDAO hotspotDAO) {
+        this.hotspotDAO = hotspotDAO;
+    }
+
+
+    @PostMapping
+    @ResponseBody
+    public int addHotspot(@RequestBody Hotspot hotspot) {
+        if (!hotspotDAO.addHotspot(hotspot)) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Hotspot Could Not Be Added");
+        }
+        return hotspot.getHid();
+    }
+
+}
---
+++ PanoTour/src/main/java/com/COSC4P02/PanoTour/entities/HotspotDAO.java
@@ -0,0 +1,52 @@
+package com.COSC4P02.PanoTour.entities;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.*;
+
+import java.util.Optional;
+
+@Repository("Hotspots")
+public class HotspotDAO {
+    private static final EntityManagerFactory ENTITY_MANAGER_FACTORY = Persistence
+            .createEntityManagerFactory("PanoTour");
+    public boolean addHotspot(Hotspot hotspot) {
+        EntityManager em = ENTITY_MANAGER_FACTORY.createEntityManager();
+        EntityTransaction entityTransaction = null;
+        boolean persisted = true;
+        try {
+            entityTransaction = em.getTransaction();
+            entityTransaction.begin();
+            em.persist(hotspot);
+            entityTransaction.commit();
+        } catch (Exception exception) {
+            if (entityTransaction != null) {
+                entityTransaction.rollback();
+                persisted = false;
+            }
+        } finally {
+            em.close();
+        }
+        return persisted;
+    }
+    public boolean deleteHotspot(Hotspot hotspot) {
+        EntityManager em = ENTITY_MANAGER_FACTORY.createEntityManager();
+        EntityTransaction entityTransaction = null;
+        boolean deleted = true;
+
+        try {
+            entityTransaction = em.getTransaction();
+            entityTransaction.begin();
+            em.remove(em.contains(hotspot) ? hotspot : em.merge(hotspot));
+            entityTransaction.commit();
+        } catch (Exception e) {
+            if (entityTransaction != null) {
+                entityTransaction.rollback();
+                deleted = false;
+            }
+        } finally {
+            em.close();
+        }
+        return deleted;
+    }
+}
