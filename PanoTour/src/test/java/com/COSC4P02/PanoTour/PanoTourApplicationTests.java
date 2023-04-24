51e69a12ba7c654fdf4ab6abc58472732a7ae265
ZachM <122565368+OkayHmm@users.noreply.github.com>
2023-04-23 22:44:31 -0400

    create first automated test, adduser

--- PanoTour/src/test/java/com/COSC4P02/PanoTour/PanoTourApplicationTests.java
+++ PanoTour/src/test/java/com/COSC4P02/PanoTour/PanoTourApplicationTests.java
@@ -3,7 +3,6 @@ package com.COSC4P02.PanoTour;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class PanoTourApplicationTests {
 
 	@Test
