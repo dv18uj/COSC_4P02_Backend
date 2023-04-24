00ee75c28d44bc90a7a5548af83d1755c7258051
D <74151412+dv18uj@users.noreply.github.com>
2023-04-23 22:28:00 -0400

    getHotspotsByPid

--- PanoTour/src/main/java/com/COSC4P02/PanoTour/entities/HotspotDAO.java
+++ PanoTour/src/main/java/com/COSC4P02/PanoTour/entities/HotspotDAO.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.*;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 @Repository("Hotspots")
@@ -29,6 +31,23 @@ public class HotspotDAO {
         }
         return persisted;
     }
+
+    public static List<Hotspot> getHotspotsByPid(int pid) {
+        EntityManager em = ENTITY_MANAGER_FACTORY.createEntityManager();
+        String query = "SELECT u FROM Panoview u WHERE u.pid=:PID";
+        TypedQuery<Hotspot> typedQuery = em.createQuery(query, Hotspot.class);
+        List<Hotspot> hotspots = Collections.emptyList();
+
+        try{
+            hotspots = typedQuery.getResultList();
+        } catch (NoResultException exception) {
+            exception.printStackTrace();
+        } finally {
+            em.close();
+        }
+        return hotspots;
+    }
+
     public boolean deleteHotspot(Hotspot hotspot) {
         EntityManager em = ENTITY_MANAGER_FACTORY.createEntityManager();
         EntityTransaction entityTransaction = null;
