d819fd0cfd95c341756d2b6ca91f8ac187a80903
ZachM <122565368+OkayHmm@users.noreply.github.com>
2023-04-26 22:23:32 -0400

    improvement, the project will now build, test still not working yet

--- PanoTour/src/main/java/com/COSC4P02/PanoTour/entities/User.java
+++ PanoTour/src/main/java/com/COSC4P02/PanoTour/entities/User.java
@@ -11,7 +11,7 @@ public class User implements Serializable
 {
     private static final long serialVersionUID = 338L;
 
-    //@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     private int uid;
 
